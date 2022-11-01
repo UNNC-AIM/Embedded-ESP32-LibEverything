52d871bcffd2029ac7d0d3c1f52b165e6883d3de
Zheng Zihao <211106370@qq.com>
2022-11-01 15:12:54 +0800

    Add transmit and receive function

--- can/can.h
+++ can/can.h
@@ -5,7 +5,9 @@
 #include <driver/gpio.h>
 #include <driver/twai.h>
 
-enum CAN_BaudRate {
+using can_frame_data = twai_message_t;
+
+enum can_baudrate {
     b25K,
     b50K,
     b100k,
@@ -17,18 +19,27 @@ enum CAN_BaudRate {
 };
 
 namespace ESP_CAN {
+    inline esp_err_t CAN_Transmit(const can_frame_data frame, TickType_t ticks_to_wait) {
+        return twai_transmit((twai_message_t*)&frame, ticks_to_wait);
+    }
 
-class CAN {
-private:
-    twai_general_config_t set_pin;
-    twai_timing_config_t set_baudrate;
-    twai_filter_config_t set_filter;
+    inline esp_err_t CAN_Receive(const can_frame_data frame, TickType_t ticks_to_wait) {
+        return twai_receive((twai_message_t*)&frame, ticks_to_wait);
+    }
+    
+    class CAN {
+        private:
+        twai_general_config_t set_pin;
+        twai_timing_config_t set_baudrate;
+        twai_filter_config_t set_filter;
 
-public:
-    void CAN_Setup(gpio_num_t TX_PIN, gpio_num_t RX_PIN, CAN_BaudRate baudrate);
-    esp_err_t CAN_Start();
-};
-
+        public:
+        void CAN_Setup(gpio_num_t TX_PIN, gpio_num_t RX_PIN, can_baudrate baudrate);
+        esp_err_t CAN_Start();
+        
+    };
 }
+
+
 
 #endif
